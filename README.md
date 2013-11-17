33f5594de08624b71259fa119694eb5d0385c5e5
Craig Steele <cr@igsteele.com>
2013-11-17 19:56:24 +0000

    first commit
    all example files

---
+++ README.md
@@ -0,0 +1,11 @@
+Noti - Simple HTML5 Desktop Notification Example
+===========
+
+This a simple example of using HTML5 Desktop Notifications
+
+###Author
+Craig Steele @Craig88
+
+###Demo
+Live demo - [cra.igsteele.com/noti](http://cra.igsteele.com/noti/)
+
---
+++ script.js
@@ -0,0 +1,49 @@
+
+	
+
+
+// Craig Steele
+// July 2013
+
+$('document').ready(main);
+
+function main() {
+
+// check for notifications support
+if (window.webkitNotifications) {
+  console.log("Notifications are supported!");
+}
+else {
+  console.log("Notifications are not supported for this Browser/OS version yet.");
+}
+
+//$('#myButton').click(function() { doThis() });
+$('#myButton').click(getPermission);
+
+}
+
+ function getPermission() {
+  if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
+    // function defined in step 2
+    console.log("You have permission, go nuts!");
+    displayNotification();
+  } else {
+    window.webkitNotifications.requestPermission();
+    console.log("You don't have permission");
+    
+  }
+}
+
+function displayNotification() {
+
+	var myNote = window.webkitNotifications.createNotification('icon.png', 'Hey dude!', 'An Awesome Desktop Notification');
+	myNote.show();
+	
+}
+
+
+
+
+
+
+  
